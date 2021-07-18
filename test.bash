1af06d665419936e892af1d7109363c7f1d95d37
Francisco Boni <boboniboni@gmail.com>
2021-07-18 09:31:56 -0300

    Small fixes & housekeeping.

--- test.bash
+++ test.bash
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+cat config | grep CONFIG_ACPI_TABLE_UPGRADE
 cat config | grep CONFIG_FORTIFY_SOURCE
 cat config | grep CONFIG_HARDENED_USERCOPY_FALLBACK
 cat config | grep CONFIG_HARDENED_USERCOPY
@@ -13,7 +14,7 @@ cat config | grep CONFIG_RANDOMIZE_MEMORY
 cat config | grep CONFIG_RANDOMIZE_BASE
 cat config | grep CONFIG_PAGE_TABLE_ISOLATION
 cat config | grep CONFIG_RETPOLINE
-cat config | grep CONFIG_DEBUG_FORCE_FUNCTION_ALIGN_32B
+# cat config | grep CONFIG_DEBUG_FORCE_FUNCTION_ALIGN_32B
 cat config | grep CONFIG_ZSWAP_DEFAULT_ON
 cat config | grep CONFIG_ZSWAP
 cat config | grep CONFIG_INTEL_TXT
@@ -41,4 +42,14 @@ cat config | grep CONFIG_DYNAMIC_DEBUG
 cat config | grep CONFIG_FRAME_POINTER
 cat config | grep CONFIG_STACK_VALIDATION
 cat config | grep CONFIG_STACKTRACE
+cat config | grep CONFIG_EFI_TEST
+cat config | grep CONFIG_DMI_SYSFS
+cat config | grep CONFIG_CGROUP_FREEZER
+cat config | grep CONFIG_X86_SYSFB
+cat config | grep CONFIG_FRAMEBUFFER_CONSOLE
+cat config | grep CONFIG_EFI_STUB
+cat config | grep CONFIG_EFI
+cat config | grep CONFIG_RELOCATABLE
+cat config | grep CONFIG_EFIVAR_FS
+cat config | grep CONFIG_EFI_VARS
 
