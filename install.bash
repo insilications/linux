62bc1639c2b8622340009281df18f3578581d833
Francisco Boni <boboniboni@gmail.com>
2021-12-04 21:00:24 -0300

    Small fixes & housekeeping.

--- install.bash
+++ install.bash
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-rpm2cpio /aot/build/clearlinux/packages/linux/rpms/linux-dev-5.15.6-1400.x86_64.rpm | (cd /; sudo cpio -i -d -u -v);
-rpm2cpio /aot/build/clearlinux/packages/linux/rpms/linux-abi-5.15.6-1400.x86_64.rpm | (cd /; sudo cpio -i -d -u -v);
-rpm2cpio /aot/build/clearlinux/packages/linux/rpms/linux-license-5.15.6-1400.x86_64.rpm | (cd /; sudo cpio -i -d -u -v);
-rpm2cpio /aot/build/clearlinux/packages/linux/rpms/linux-extra-5.15.6-1400.x86_64.rpm | (cd /; sudo cpio -i -d -u -v);
-rpm2cpio /aot/build/clearlinux/packages/linux/rpms/linux-5.15.6-1400.x86_64.rpm | (cd /; sudo cpio -i -d -u -v);
-rpm2cpio /aot/build/clearlinux/packages/linux/rpms/linux-applications-5.15.6-1400.x86_64.rpm | (cd /; sudo cpio -i -d -u -v);
+rpm2cpio /aot/build/clearlinux/packages/linux/rpms/linux-dev-5.15.6-1500.x86_64.rpm | (cd /; sudo cpio -i -d -u -v);
+rpm2cpio /aot/build/clearlinux/packages/linux/rpms/linux-abi-5.15.6-1500.x86_64.rpm | (cd /; sudo cpio -i -d -u -v);
+rpm2cpio /aot/build/clearlinux/packages/linux/rpms/linux-license-5.15.6-1500.x86_64.rpm | (cd /; sudo cpio -i -d -u -v);
+rpm2cpio /aot/build/clearlinux/packages/linux/rpms/linux-extra-5.15.6-1500.x86_64.rpm | (cd /; sudo cpio -i -d -u -v);
+rpm2cpio /aot/build/clearlinux/packages/linux/rpms/linux-5.15.6-1500.x86_64.rpm | (cd /; sudo cpio -i -d -u -v);
+rpm2cpio /aot/build/clearlinux/packages/linux/rpms/linux-applications-5.15.6-1500.x86_64.rpm | (cd /; sudo cpio -i -d -u -v);
