82cb94df28cf4ef0841c632743b8797d0c6bf56d
Francisco Boni Neto <boboniboni@gmail.com>
2022-07-13 12:44:16 -0300

    Small fixes & housekeeping.

--- test.bash
+++ test.bash
@@ -1,52 +1,59 @@
 #!/usr/bin/env bash
-scripts/config --state CONFIG_ACPI_TABLE_UPGRADE
-scripts/config --state CONFIG_EFI_TEST
-scripts/config --state CONFIG_EFI_STUB
-scripts/config --state CONFIG_EFI
-scripts/config --state CONFIG_EFIVAR_FS
-scripts/config --state CONFIG_EFI_VARS
-scripts/config --state CONFIG_FORTIFY_SOURCE
-scripts/config --state CONFIG_HARDENED_USERCOPY_FALLBACK
-scripts/config --state CONFIG_HARDENED_USERCOPY
-scripts/config --state CONFIG_SLAB_FREELIST_RANDOM
-scripts/config --state CONFIG_SLAB_FREELIST_HARDENED
-scripts/config --state CONFIG_STACKPROTECTOR_STRONG
-scripts/config --state CONFIG_STACKPROTECTOR
-scripts/config --state CONFIG_HAVE_STACKPROTECTOR
-scripts/config --state CONFIG_CC_HAS_SANE_STACKPROTECTOR
-scripts/config --state CONFIG_RANDOMIZE_MEMORY
-scripts/config --state CONFIG_RANDOMIZE_BASE
-scripts/config --state CONFIG_PAGE_TABLE_ISOLATION
-scripts/config --state CONFIG_RETPOLINE
-scripts/config --state CONFIG_DEBUG_FORCE_FUNCTION_ALIGN_32B
-scripts/config --state CONFIG_ZSWAP_DEFAULT_ON
-scripts/config --state CONFIG_ZSWAP
-scripts/config --state CONFIG_INTEL_TXT
-scripts/config --state CONFIG_MICROCODE
-scripts/config --state CONFIG_MICROCODE_INTEL
-scripts/config --state CONFIG_MICROCODE_AMD
-scripts/config --state CONFIG_MICROCODE_OLD_INTERFACE
-scripts/config --state CONFIG_X86_SMAP
-scripts/config --state CONFIG_X86_SGX
-scripts/config --state CONFIG_X86_INTEL_TSX_MODE_ON
-scripts/config --state CONFIG_X86_INTEL_TSX_MODE_AUTO
-scripts/config --state CONFIG_DEBUG_KERNEL
-scripts/config --state CONFIG_UNWINDER_ORC
-scripts/config --state CONFIG_PAGE_POISONING
-scripts/config --state CONFIG_DEBUG_NOTIFIERS
-scripts/config --state CONFIG_DEBUG_SG
-scripts/config --state CONFIG_SHUFFLE_PAGE_ALLOCATOR
-scripts/config --state CONFIG_REFCOUNT_FULL
-scripts/config --state CONFIG_VMAP_STACK
-scripts/config --state CONFIG_KSM
-scripts/config --state CONFIG_ZRAM_WRITEBACK
-scripts/config --state CONFIG_ZRAM
-scripts/config --state SCHED_STACK_END_CHECK
-scripts/config --state CONFIG_DYNAMIC_DEBUG
-scripts/config --state CONFIG_FRAME_POINTER
-scripts/config --state CONFIG_STACK_VALIDATION
-scripts/config --state CONFIG_STACKTRACE
-scripts/config --state CONFIG_DMI_SYSFS
-scripts/config --state CONFIG_CGROUP_FREEZER
-scripts/config --state CONFIG_FRAMEBUFFER_CONSOLE
-scripts/config --state CONFIG_RELOCATABLE
+./configtool --file /insilications/apps/linux-5.19.0/.config --state CONFIG_ACPI_TABLE_UPGRADE
+./configtool --file /insilications/apps/linux-5.19.0/.config --state CONFIG_DMI_SYSFS
+./configtool --file /insilications/apps/linux-5.19.0/.config --state CONFIG_EFI_TEST
+./configtool --file /insilications/apps/linux-5.19.0/.config --state CONFIG_EFI_STUB
+./configtool --file /insilications/apps/linux-5.19.0/.config --state CONFIG_EFI
+./configtool --file /insilications/apps/linux-5.19.0/.config --state CONFIG_EFIVAR_FS
+./configtool --file /insilications/apps/linux-5.19.0/.config --state CONFIG_EFI_VARS
+./configtool --file /insilications/apps/linux-5.19.0/.config --state CONFIG_FORTIFY_SOURCE
+./configtool --file /insilications/apps/linux-5.19.0/.config --state CONFIG_HARDENED_USERCOPY_FALLBACK
+./configtool --file /insilications/apps/linux-5.19.0/.config --state CONFIG_HARDENED_USERCOPY
+./configtool --file /insilications/apps/linux-5.19.0/.config --state CONFIG_SLAB_FREELIST_RANDOM
+./configtool --file /insilications/apps/linux-5.19.0/.config --state CONFIG_SLAB_FREELIST_HARDENED
+./configtool --file /insilications/apps/linux-5.19.0/.config --state CONFIG_STACKPROTECTOR_STRONG
+./configtool --file /insilications/apps/linux-5.19.0/.config --state CONFIG_STACKPROTECTOR
+./configtool --file /insilications/apps/linux-5.19.0/.config --state CONFIG_HAVE_STACKPROTECTOR
+./configtool --file /insilications/apps/linux-5.19.0/.config --state CONFIG_CC_HAS_SANE_STACKPROTECTOR
+./configtool --file /insilications/apps/linux-5.19.0/.config --state CONFIG_RANDOMIZE_MEMORY
+./configtool --file /insilications/apps/linux-5.19.0/.config --state CONFIG_RANDOMIZE_BASE
+./configtool --file /insilications/apps/linux-5.19.0/.config --state CONFIG_PAGE_TABLE_ISOLATION
+./configtool --file /insilications/apps/linux-5.19.0/.config --state PAGE_TABLE_ISOLATION
+./configtool --file /insilications/apps/linux-5.19.0/.config --state CONFIG_RETPOLINE
+./configtool --file /insilications/apps/linux-5.19.0/.config --state CPU_IBPB_ENTRY
+./configtool --file /insilications/apps/linux-5.19.0/.config --state CPU_IBRS_ENTRY
+./configtool --file /insilications/apps/linux-5.19.0/.config --state SPECULATION_MITIGATIONS
+./configtool --file /insilications/apps/linux-5.19.0/.config --state SLS
+./configtool --file /insilications/apps/linux-5.19.0/.config --state HZ_750
+./configtool --file /insilications/apps/linux-5.19.0/.config --state CONFIG_DEBUG_FORCE_FUNCTION_ALIGN_32B
+./configtool --file /insilications/apps/linux-5.19.0/.config --state CONFIG_DEBUG_FORCE_FUNCTION_ALIGN_64B
+./configtool --file /insilications/apps/linux-5.19.0/.config --state CONFIG_ZSWAP_DEFAULT_ON
+./configtool --file /insilications/apps/linux-5.19.0/.config --state CONFIG_ZSWAP
+./configtool --file /insilications/apps/linux-5.19.0/.config --state CONFIG_INTEL_TXT
+./configtool --file /insilications/apps/linux-5.19.0/.config --state CONFIG_MICROCODE
+./configtool --file /insilications/apps/linux-5.19.0/.config --state CONFIG_MICROCODE_INTEL
+./configtool --file /insilications/apps/linux-5.19.0/.config --state CONFIG_MICROCODE_AMD
+./configtool --file /insilications/apps/linux-5.19.0/.config --state CONFIG_MICROCODE_OLD_INTERFACE
+./configtool --file /insilications/apps/linux-5.19.0/.config --state CONFIG_X86_SMAP
+./configtool --file /insilications/apps/linux-5.19.0/.config --state CONFIG_X86_SGX
+./configtool --file /insilications/apps/linux-5.19.0/.config --state CONFIG_X86_INTEL_TSX_MODE_ON
+./configtool --file /insilications/apps/linux-5.19.0/.config --state CONFIG_X86_INTEL_TSX_MODE_AUTO
+./configtool --file /insilications/apps/linux-5.19.0/.config --state CONFIG_DEBUG_KERNEL
+./configtool --file /insilications/apps/linux-5.19.0/.config --state CONFIG_UNWINDER_ORC
+./configtool --file /insilications/apps/linux-5.19.0/.config --state CONFIG_PAGE_POISONING
+./configtool --file /insilications/apps/linux-5.19.0/.config --state CONFIG_DEBUG_NOTIFIERS
+./configtool --file /insilications/apps/linux-5.19.0/.config --state CONFIG_DEBUG_SG
+./configtool --file /insilications/apps/linux-5.19.0/.config --state CONFIG_SHUFFLE_PAGE_ALLOCATOR
+./configtool --file /insilications/apps/linux-5.19.0/.config --state CONFIG_REFCOUNT_FULL
+./configtool --file /insilications/apps/linux-5.19.0/.config --state CONFIG_VMAP_STACK
+./configtool --file /insilications/apps/linux-5.19.0/.config --state CONFIG_KSM
+./configtool --file /insilications/apps/linux-5.19.0/.config --state CONFIG_ZRAM_WRITEBACK
+./configtool --file /insilications/apps/linux-5.19.0/.config --state CONFIG_ZRAM
+./configtool --file /insilications/apps/linux-5.19.0/.config --state SCHED_STACK_END_CHECK
+./configtool --file /insilications/apps/linux-5.19.0/.config --state CONFIG_DYNAMIC_DEBUG
+./configtool --file /insilications/apps/linux-5.19.0/.config --state CONFIG_FRAME_POINTER
+./configtool --file /insilications/apps/linux-5.19.0/.config --state CONFIG_STACK_VALIDATION
+./configtool --file /insilications/apps/linux-5.19.0/.config --state CONFIG_STACKTRACE
+./configtool --file /insilications/apps/linux-5.19.0/.config --state CONFIG_CGROUP_FREEZER
+./configtool --file /insilications/apps/linux-5.19.0/.config --state CONFIG_FRAMEBUFFER_CONSOLE
+./configtool --file /insilications/apps/linux-5.19.0/.config --state CONFIG_RELOCATABLE
