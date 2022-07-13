3d8477e81dc8cd70a8d40446b7ab0aa33ed36db6
Francisco Boni Neto <boboniboni@gmail.com>
2022-07-13 11:21:56 -0300

    Small fixes & housekeeping.

--- test.bash
+++ test.bash
@@ -1,52 +1,52 @@
 #!/usr/bin/env bash
-cat config | grep CONFIG_ACPI_TABLE_UPGRADE
-cat config | grep CONFIG_EFI_TEST
-cat config | grep CONFIG_EFI_STUB
-cat config | grep CONFIG_EFI=
-cat config | grep CONFIG_EFIVAR_FS
-cat config | grep CONFIG_EFI_VARS
-cat config | grep CONFIG_FORTIFY_SOURCE
-cat config | grep CONFIG_HARDENED_USERCOPY_FALLBACK
-cat config | grep CONFIG_HARDENED_USERCOPY
-cat config | grep CONFIG_SLAB_FREELIST_RANDOM
-cat config | grep CONFIG_SLAB_FREELIST_HARDENED
-cat config | grep CONFIG_STACKPROTECTOR_STRONG
-cat config | grep CONFIG_STACKPROTECTOR
-cat config | grep CONFIG_HAVE_STACKPROTECTOR
-cat config | grep CONFIG_CC_HAS_SANE_STACKPROTECTOR
-cat config | grep CONFIG_RANDOMIZE_MEMORY
-cat config | grep CONFIG_RANDOMIZE_BASE
-cat config | grep CONFIG_PAGE_TABLE_ISOLATION
-cat config | grep CONFIG_RETPOLINE
-# cat config | grep CONFIG_DEBUG_FORCE_FUNCTION_ALIGN_32B
-cat config | grep CONFIG_ZSWAP_DEFAULT_ON
-cat config | grep CONFIG_ZSWAP
-cat config | grep CONFIG_INTEL_TXT
-cat config | grep CONFIG_MICROCODE
-cat config | grep CONFIG_MICROCODE_INTEL
-cat config | grep CONFIG_MICROCODE_AMD
-cat config | grep CONFIG_MICROCODE_OLD_INTERFACE
-cat config | grep CONFIG_X86_SMAP
-cat config | grep CONFIG_X86_SGX
-cat config | grep CONFIG_X86_INTEL_TSX_MODE_ON
-cat config | grep CONFIG_X86_INTEL_TSX_MODE_AUTO
-cat config | grep CONFIG_DEBUG_KERNEL
-cat config | grep CONFIG_UNWINDER_ORC
-cat config | grep CONFIG_PAGE_POISONING
-cat config | grep CONFIG_DEBUG_NOTIFIERS
-cat config | grep CONFIG_DEBUG_SG
-cat config | grep CONFIG_SHUFFLE_PAGE_ALLOCATOR
-cat config | grep CONFIG_REFCOUNT_FULL
-cat config | grep CONFIG_VMAP_STACK
-cat config | grep CONFIG_KSM
-cat config | grep CONFIG_ZRAM_WRITEBACK
-cat config | grep CONFIG_ZRAM
-cat config | grep SCHED_STACK_END_CHECK
-cat config | grep CONFIG_DYNAMIC_DEBUG
-cat config | grep CONFIG_FRAME_POINTER
-cat config | grep CONFIG_STACK_VALIDATION
-cat config | grep CONFIG_STACKTRACE
-cat config | grep CONFIG_DMI_SYSFS
-cat config | grep CONFIG_CGROUP_FREEZER
-cat config | grep CONFIG_FRAMEBUFFER_CONSOLE
-cat config | grep CONFIG_RELOCATABLE
+scripts/config --state CONFIG_ACPI_TABLE_UPGRADE
+scripts/config --state CONFIG_EFI_TEST
+scripts/config --state CONFIG_EFI_STUB
+scripts/config --state CONFIG_EFI
+scripts/config --state CONFIG_EFIVAR_FS
+scripts/config --state CONFIG_EFI_VARS
+scripts/config --state CONFIG_FORTIFY_SOURCE
+scripts/config --state CONFIG_HARDENED_USERCOPY_FALLBACK
+scripts/config --state CONFIG_HARDENED_USERCOPY
+scripts/config --state CONFIG_SLAB_FREELIST_RANDOM
+scripts/config --state CONFIG_SLAB_FREELIST_HARDENED
+scripts/config --state CONFIG_STACKPROTECTOR_STRONG
+scripts/config --state CONFIG_STACKPROTECTOR
+scripts/config --state CONFIG_HAVE_STACKPROTECTOR
+scripts/config --state CONFIG_CC_HAS_SANE_STACKPROTECTOR
+scripts/config --state CONFIG_RANDOMIZE_MEMORY
+scripts/config --state CONFIG_RANDOMIZE_BASE
+scripts/config --state CONFIG_PAGE_TABLE_ISOLATION
+scripts/config --state CONFIG_RETPOLINE
+scripts/config --state CONFIG_DEBUG_FORCE_FUNCTION_ALIGN_32B
+scripts/config --state CONFIG_ZSWAP_DEFAULT_ON
+scripts/config --state CONFIG_ZSWAP
+scripts/config --state CONFIG_INTEL_TXT
+scripts/config --state CONFIG_MICROCODE
+scripts/config --state CONFIG_MICROCODE_INTEL
+scripts/config --state CONFIG_MICROCODE_AMD
+scripts/config --state CONFIG_MICROCODE_OLD_INTERFACE
+scripts/config --state CONFIG_X86_SMAP
+scripts/config --state CONFIG_X86_SGX
+scripts/config --state CONFIG_X86_INTEL_TSX_MODE_ON
+scripts/config --state CONFIG_X86_INTEL_TSX_MODE_AUTO
+scripts/config --state CONFIG_DEBUG_KERNEL
+scripts/config --state CONFIG_UNWINDER_ORC
+scripts/config --state CONFIG_PAGE_POISONING
+scripts/config --state CONFIG_DEBUG_NOTIFIERS
+scripts/config --state CONFIG_DEBUG_SG
+scripts/config --state CONFIG_SHUFFLE_PAGE_ALLOCATOR
+scripts/config --state CONFIG_REFCOUNT_FULL
+scripts/config --state CONFIG_VMAP_STACK
+scripts/config --state CONFIG_KSM
+scripts/config --state CONFIG_ZRAM_WRITEBACK
+scripts/config --state CONFIG_ZRAM
+scripts/config --state SCHED_STACK_END_CHECK
+scripts/config --state CONFIG_DYNAMIC_DEBUG
+scripts/config --state CONFIG_FRAME_POINTER
+scripts/config --state CONFIG_STACK_VALIDATION
+scripts/config --state CONFIG_STACKTRACE
+scripts/config --state CONFIG_DMI_SYSFS
+scripts/config --state CONFIG_CGROUP_FREEZER
+scripts/config --state CONFIG_FRAMEBUFFER_CONSOLE
+scripts/config --state CONFIG_RELOCATABLE
