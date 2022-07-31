f85e38ee6d1bebfb08a2bafe7fa612f6173c9110
Francisco Boni Neto <boboniboni@gmail.com>
2022-07-31 13:07:04 -0300

    Small fixes & housekeeping.

--- install.bash
+++ install.bash
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-rpm2cpio /aot/build/clearlinux/packages/linux/rpms/linux-dev-5.19.0-1917.x86_64.rpm | (cd /; sudo cpio -i -d -u -v);
-rpm2cpio /aot/build/clearlinux/packages/linux/rpms/linux-abi-5.19.0-1917.x86_64.rpm | (cd /; sudo cpio -i -d -u -v);
-rpm2cpio /aot/build/clearlinux/packages/linux/rpms/linux-cpio-5.19.0-1917.x86_64.rpm | (cd /; sudo cpio -i -d -u -v);
-rpm2cpio /aot/build/clearlinux/packages/linux/rpms/linux-license-5.19.0-1917.x86_64.rpm | (cd /; sudo cpio -i -d -u -v);
-rpm2cpio /aot/build/clearlinux/packages/linux/rpms/linux-extra-5.19.0-1917.x86_64.rpm | (cd /; sudo cpio -i -d -u -v);
-rpm2cpio /aot/build/clearlinux/packages/linux/rpms/linux-5.19.0-1917.x86_64.rpm | (cd /; sudo cpio -i -d -u -v);
-rpm2cpio /aot/build/clearlinux/packages/linux/rpms/linux-applications-5.19.0-1917.x86_64.rpm | (cd /; sudo cpio -i -d -u -v);
+rpm2cpio /aot/build/clearlinux/packages/linux/rpms/linux-dev-5.19.0-1918.x86_64.rpm | (cd /; sudo cpio -i -d -u -v);
+rpm2cpio /aot/build/clearlinux/packages/linux/rpms/linux-abi-5.19.0-1918.x86_64.rpm | (cd /; sudo cpio -i -d -u -v);
+rpm2cpio /aot/build/clearlinux/packages/linux/rpms/linux-cpio-5.19.0-1918.x86_64.rpm | (cd /; sudo cpio -i -d -u -v);
+rpm2cpio /aot/build/clearlinux/packages/linux/rpms/linux-license-5.19.0-1918.x86_64.rpm | (cd /; sudo cpio -i -d -u -v);
+rpm2cpio /aot/build/clearlinux/packages/linux/rpms/linux-extra-5.19.0-1918.x86_64.rpm | (cd /; sudo cpio -i -d -u -v);
+rpm2cpio /aot/build/clearlinux/packages/linux/rpms/linux-5.19.0-1918.x86_64.rpm | (cd /; sudo cpio -i -d -u -v);
+rpm2cpio /aot/build/clearlinux/packages/linux/rpms/linux-applications-5.19.0-1918.x86_64.rpm | (cd /; sudo cpio -i -d -u -v);
